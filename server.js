214f8736e216fdd9b8e794939477df3ce66d4fa0
KoushikwebDev <kou.saha.ami@gmail.com>
2022-12-15 23:30:57 +0530

    bug fixed

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const { PORT } = process.env;
 import app from "./app.js";
 
 app.listen(PORT, () => {
-  console.log(`App is running at port ${PORT}`);
+  console.log(`Server is running at port ${PORT}`);
 });
